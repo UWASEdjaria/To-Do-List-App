ed03946eba6ffbfb4bb87e65db193ec5d93759c2
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-20 23:37:49 +0200

    add addTask function

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-let head=document.getElementById('head');
-let input=document.getElementById('TaskInput');
-let button=document.getElementById('button');
+
+let head = document.getElementById('head');
+let input = document.getElementById('TaskInput');
+let button = document.getElementById('button');
+let taskList = document.getElementById('TaskList');
+
+function addTask() {
+    const TaskInput = inputField.value.trim();
+    if (TaskList == "") {
+        alert("Task Name is Required")
+        return;
+    }};
