b319fd217538139929e2267b11c1ae1a55392737
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-21 19:37:10 +0200

    add eventlisterner to delete button and span

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ let deleteBtn = document.createElement("button");
 deleteBtn.textContent = "Delete All";
 deleteBtn.className = "w-full mt-2 bg-red-500 text-white p-2 rounded-lg hover:bg-red-600";
 
-
+function addTask() {
+    const TaskInput = input.value.trim();
+    if (taskList == "") {
+        alert("Task Name is Required")
+        return;
+}};
