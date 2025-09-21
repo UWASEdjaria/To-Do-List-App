ef5767060afdb08fc587bd4265357ce32408e44c
Djaria UWASE <uwasedjaria41@gmail.com>
2025-09-21 19:01:10 +0200

    add delete button

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
-
-let head = document.getElementById('head');
 let input = document.getElementById('TaskInput');
 let button = document.getElementById('button');
 let taskList = document.getElementById('TaskList');
 
-function addTask() {
-    const TaskInput = inputField.value.trim();
-    if (TaskList == "") {
-        alert("Task Name is Required")
-        return;
-    }
-    
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const deleteButton = document.createElement("deleteButton");
 
-    deleteButton.classList.add("delete-btn")
-    deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", function () {
-       TaskList.removeChild(li)
-    })
-       span.textContent = textText;
-
-    span.addEventListener("click", () => {
-        li.classList.toggle("line-through")
-    })
-     li.appendChild(span);
-    li.appendChild(deleteButton);
-
-    
-    taskList.appendChild(li);
-    inputField.value = "";
-
-    
-inputField.addEventListener("keydown", (e) => {
-    if (e.key == "Enter") {
-        addTask()
-    }})
+let deleteBtn = document.createElement("button");
+deleteBtn.textContent = "Delete All";
+deleteBtn.className = "w-full mt-2 bg-red-500 text-white p-2 rounded-lg hover:bg-red-600";
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-};
